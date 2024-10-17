8e4c6a638804e9e166a45330bf2ebdb20f006392
Sergio Reyes <82613689+sreyes25@users.noreply.github.com>
2024-10-16 22:18:50 -0400

    Initial Commit

---
+++ InvoicingApp/InvoicingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoicingAppApp.swift
+//  InvoicingApp
+//
+//  Created by Sergio Reyes on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct InvoicingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InvoicingAppTests/InvoicingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  InvoicingAppTests.swift
+//  InvoicingAppTests
+//
+//  Created by Sergio Reyes on 10/16/24.
+//
+
+import Testing
+@testable import InvoicingApp
+
+struct InvoicingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
